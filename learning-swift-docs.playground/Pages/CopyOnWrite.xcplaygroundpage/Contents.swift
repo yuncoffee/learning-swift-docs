b8f66df6b6187451af19ec7e20e1ad99c72426fc
yuncoffee <dong072815@gmail.com>
2023-06-03 17:00:04 +0900

    practice: String 구조체에서의 메모리 복사

---
+++ learning-swift-docs.playground/Pages/CopyOnWrite.xcplaygroundpage/Contents.swift
@@ -0,0 +1,99 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//MARK: struct - COW(copy-on-write)
+
+func address(of object: UnsafeRawPointer) -> String{
+    let address = Int(bitPattern: object)
+    return String(format: "%p", address)
+}
+
+print(MemoryLayout<String>.size)  // String 구조체의 메모리 값은 16이다.
+
+//MARK: 메모리의 참조값을 복사한다.
+var myLongString: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var myLongString2 = myLongString
+
+address(of: myLongString)
+address(of: myLongString2)
+
+myLongString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+address(of: myLongString) // 값이 다른 값으로 바뀌지 않았을 때 메모리값이 바뀌지 않는다.
+
+myLongString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX" // YZ 제거
+myLongString2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW" // XYZ 제거
+
+address(of: myLongString) // 값이 다른 값으로 바뀌었을 때 메모리값이 바뀌지 않는다.
+address(of: myLongString2) // 값이 다른 값으로 바뀌었을 때 메모리값이 바뀌지 않는다.
+print(MemoryLayout.size(ofValue: myLongString))
+print(MemoryLayout.size(ofValue: myLongString2))
+
+//MARK: COW(Copy-on-Write) - 일반적인 컬렉션 타입에서의 동작
+var myMidString: String = "abcdefghijklmnopqrstuvwxyzABCDEF" // 32자
+var myMidString2 = myMidString
+address(of: myMidString)
+address(of: myMidString2)
+
+myMidString = "abcdefghijklmnopqrstuvwxyzABCDEF"
+myMidString2 = "abcdefghijklmnopqrstuvwxyzABCDE"
+address(of: myMidString) // 값이 다른 값으로 바뀌지 않았을 때 메모리값이 바뀌지 않는다.
+address(of: myMidString2) // 값이 다른 값으로 바뀌었을 때 메모리값이 바뀐다.
+print(MemoryLayout.size(ofValue: myMidString))
+print(MemoryLayout.size(ofValue: myMidString2))
+
+//MARK: 즉시 메모리를 복사한다.
+var myString: String = "Hello"
+var myString2 = myString
+address(of: myString)
+address(of: myString2)
+myString = "Hello"
+myString2 = "World"
+
+address(of: myString) // 값이 다른 값으로 바뀌지 않았을 때 메모리값이 바뀐다.
+address(of: myString2) // 값이 다른 값으로 바뀌었을 때 메모리값이 바뀐다.
+print(MemoryLayout.size(ofValue: myString))
+print(MemoryLayout.size(ofValue: myString2))
+
+
+//var myArray = [1, 2, 3]
+//var myArray2 = myArray
+//
+//var mySet: Set = [1, 2, 2]
+//var mySet2: Set = mySet
+//
+//var myInt = 1
+//var myInt2 = myInt
+//
+//address(of: myArray)
+//address(of: myArray2)
+//
+//myArray[2] = 10
+//address(of: myArray)
+//address(of: myArray2)
+//
+//address(of: &mySet)
+//address(of: &mySet2)
+//
+//address(of: &myInt)
+//address(of: &myInt2)
+//
+//myInt + 1
+//address(of: &myInt)
+//
+//myInt = 10
+//address(of: &myInt)
+//
+//myInt = myInt2
+//myInt2 = myInt
+//
+//myArray2[1] = 2
+//
+//address(of: myArray)
+//address(of: myArray2)
+//
+//mySet.insert(2)
+//mySet.insert(3)
+//
+//address(of: &mySet)
+//address(of: &mySet2)
--- learning-swift-docs.playground/Pages/StructuresAndClasses.xcplaygroundpage/Contents.swift
+++ learning-swift-docs.playground/Pages/StructuresAndClasses.xcplaygroundpage/Contents.swift
@@ -2,115 +2,11 @@ import Foundation
 import UIKit
 import SwiftUI
 
-//MARK: struct - COW(copy-on-write)
-/**
- func address(of object: UnsafeRawPointer) -> String{
-     let address = Int(bitPattern: object)
-     return String(format: "%p", address)
- }
-
- var myString: String = "Hello"
- var myString2 = myString
-
- var myLongString: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
- var myLongString2 = myLongString
-
- var myArray = [1, 2, 3]
- var myArray2 = myArray
-
- var mySet: Set = [1, 2, 2]
- var mySet2: Set = mySet
-
- var myInt = 1
- var myInt2 = myInt
- print(MemoryLayout<String>.size) // String 구조체의 메모리 값은 16이다.
-
- address(of: myString)
- address(of: myString2)
- print(MemoryLayout.size(ofValue: myString))
- print(MemoryLayout.size(ofValue: myString2))
- address(of: myLongString)
- address(of: myLongString2)
- myLongString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ2"
- myLongString2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV"
- address(of: myLongString)
- address(of: myLongString2)
- print(MemoryLayout.size(ofValue: myLongString))
- print(MemoryLayout.size(ofValue: myLongString2))
- myString = "World"
- address(of: myString)
- address(of: myString2)
- address(of: myString)
- address(of: myString2)
-
- address(of: myArray)
- address(of: myArray2)
-
- myArray[2] = 10
- address(of: myArray)
- address(of: myArray2)
-
- address(of: &mySet)
- address(of: &mySet2)
-
- address(of: &myInt)
- address(of: &myInt2)
-
- myInt + 1
- address(of: &myInt)
-
- myInt = 10
- address(of: &myInt)
-
- myInt = myInt2
- myInt2 = myInt
-
- myArray2[1] = 2
-
- address(of: myArray)
- address(of: myArray2)
-
- mySet.insert(2)
- mySet.insert(3)
-
- address(of: &mySet)
- address(of: &mySet2)
- */
-
 //MARK: class & struct
-
 func address(of object: UnsafeRawPointer) -> String{
     let address = Int(bitPattern: object)
     return String(format: "%p", address)
 }
-
-
-var myString: String = "Hello"
-var myString2 = myString
-
-var myLongString: String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var myLongString2 = myLongString
-
-address(of: myString)
-address(of: myString2)
-print(MemoryLayout.size(ofValue: myString))
-print(MemoryLayout.size(ofValue: myString2))
-address(of: myLongString)
-address(of: myLongString2)
-myLongString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ2"
-myLongString2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW"
-address(of: myLongString)
-address(of: myLongString2)
-print(MemoryLayout.size(ofValue: myLongString))
-print(MemoryLayout.size(ofValue: myLongString2))
-myString = "World"
-address(of: myString)
-address(of: myString2)
-address(of: myString)
-address(of: myString)
-
-address(of: myString2)
-address(of: myLongString)
 
 struct Resolution {
     var width = 0
