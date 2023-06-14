9a6624f6053fefe4e7e05dc578e3d2f46ee8ac8a
yuncoffee <dong072815@gmail.com>
2023-06-15 00:51:41 +0900

    study: read-only properties

--- learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -191,3 +191,97 @@ import Foundation
  print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
  // Prints "square.origin is now at (10.0, 10.0)"
  */
+
+// MARK: Shorthand
+/**
+ struct Point {
+     var x = 0.0, y = 0.0
+ }
+ struct Size {
+     var width = 0.0, height = 0.0
+ }
+ struct Rect {
+     var origin = Point()
+     var size = Size()
+     var center: Point {
+         get {
+             let centerX = origin.x + (size.width / 2)
+             let centerY = origin.y + (size.height / 2)
+             return Point(x: centerX, y: centerY)
+         }
+         set(newCenter) {
+             origin.x = newCenter.x - (size.width / 2)
+             origin.y = newCenter.y - (size.height / 2)
+         }
+     }
+ }
+
+ struct AlternativeRect {
+     var origin = Point()
+     var size = Size()
+     var center: Point {
+         get {
+             let centerX = origin.x + (size.width / 2)
+             let centerY = origin.y + (size.height / 2)
+             return Point(x: centerX, y: centerY)
+         }
+         set {
+             origin.x = newValue.x - (size.width / 2)
+             origin.y = newValue.y - (size.height / 2)
+         }
+     }
+ }
+
+ struct CompactRect {
+     var origin = Point()
+     var size = Size()
+     var center: Point {
+         get {
+             Point(x: origin.x + (size.width / 2),
+                   y: origin.y + (size.height / 2))
+         }
+         set {
+             origin.x = newValue.x - (size.width / 2)
+             origin.y = newValue.y - (size.height / 2)
+         }
+     }
+ }
+
+ */
+
+// MARK: Read-Only Computed Properties
+/**
+ struct Cuboid {
+     var width = 0.0, height = 0.0, depth = 0.0
+     var volume: Double {
+         return width * height * depth
+     }
+ }
+
+ struct alterGetCuboid {
+     var width = 0.0
+     var height = 0.0
+     var depth = 0.0
+     var volume: Double {
+         get { width * height * depth }
+     }
+ }
+
+ struct alterCuboid {
+     var width = 0.0
+     var height = 0.0
+     var depth = 0.0
+     var volume: Double {
+         width * height * depth
+     }
+ }
+
+ let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+ let alterFourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+
+ print(alterFourByFiveByTwo.volume)
+ print(fourByFiveByTwo.volume)
+ print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+ // Prints "the volume of fourByFiveByTwo is 40.0"
+
+ */
