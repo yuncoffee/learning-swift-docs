4ffa063baf2c5e02e8ceef360bd915ec3819997c
yuncoffee <dong072815@gmail.com>
2023-06-16 21:20:40 +0900

    study: Projected Value

--- learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -315,52 +315,206 @@ import Foundation
 
 
 //MARK: Property Wrapper
+/**
+ @propertyWrapper
+ struct TwelveOrLess {
+     private var number = 0
+     var wrappedValue: Int {
+         get { return number }
+         set { number = min(newValue, 12) }
+     }
+ }
 
-@propertyWrapper
-struct TwelveOrLess {
-    private var number = 0
-    var wrappedValue: Int {
-        get { return number }
-        set { number = min(newValue, 12) }
-    }
-}
+ var a = TwelveOrLess()
+ print(a.wrappedValue)
+ a.wrappedValue = 15
+ a.wrappedValue
+ print(a.wrappedValue)
 
-var a = TwelveOrLess()
-print(a.wrappedValue)
-a.wrappedValue = 15
-a.wrappedValue
-print(a.wrappedValue)
+ struct SmallRectangle {
+     @TwelveOrLess var height: Int
+     @TwelveOrLess var width: Int
+ }
 
-struct SmallRectangle {
-    @TwelveOrLess var height: Int
-    @TwelveOrLess var width: Int
-}
-
-struct SmallRectangle2 {
-    private var _height = TwelveOrLess()
-    private var _width = TwelveOrLess()
-    var height: Int {
-        get { return _height.wrappedValue }
-        set { _height.wrappedValue = newValue }
-    }
-    var width: Int {
-        get { return _width.wrappedValue }
-        set { _width.wrappedValue = newValue }
-    }
-}
-
-
-//var rectangle = SmallRectangle()
-var rectangle = SmallRectangle2()
-print(rectangle.height)
-// Prints "0"
-
-rectangle.height = 10
-print(rectangle.height)
-// Prints "10"
-
-rectangle.height = 24
-print(rectangle.height)
-// Prints "12"
+ struct SmallRectangle2 {
+     private var _height = TwelveOrLess()
+     private var _width = TwelveOrLess()
+     var height: Int {
+         get { return _height.wrappedValue }
+         set { _height.wrappedValue = newValue }
+     }
+     var width: Int {
+         get { return _width.wrappedValue }
+         set { _width.wrappedValue = newValue }
+     }
+ }
 
 
+ //var rectangle = SmallRectangle()
+ var rectangle = SmallRectangle2()
+ print(rectangle.height)
+ // Prints "0"
+
+ rectangle.height = 10
+ print(rectangle.height)
+ // Prints "10"
+
+ rectangle.height = 24
+ print(rectangle.height)
+ // Prints "12"
+ */
+
+
+// MARK: Setting Initial Values for Wrapped Properties
+
+/**
+ @propertyWrapper
+ struct SmallNumber {
+     private var maximum: Int
+     private var number: Int
+
+     var wrappedValue: Int {
+         get { return number }
+         set { number = min(newValue, maximum) }
+     }
+
+     init() {
+         maximum = 12
+         number = 0
+     }
+     init(wrappedValue: Int) {
+         maximum = 12
+         number = min(wrappedValue, maximum)
+     }
+     init(wrappedValue: Int, maximum: Int) {
+         self.maximum = maximum
+         number = min(wrappedValue, maximum)
+     }
+ }
+
+ struct ZeroRectangle {
+     @SmallNumber var height: Int // 이니셜라이저를 사용하지 않고 선언
+     @SmallNumber var width: Int
+ }
+
+ var zeroRectangle = ZeroRectangle()
+ print(zeroRectangle.height, zeroRectangle.width)
+ // Prints "0 0"
+
+
+ struct UnitRectangle {
+     @SmallNumber var height: Int = 1
+     @SmallNumber var width: Int = 1
+ }
+
+ var unitRectangle = UnitRectangle()
+ print(unitRectangle.height, unitRectangle.width)
+ // Prints "1 1"
+
+ struct NarrowRectangle {
+     @SmallNumber(wrappedValue: 2, maximum: 5) var height: Int
+     @SmallNumber(wrappedValue: 3, maximum: 4) var width: Int
+ }
+
+ struct TestRectangle {
+     // @SmallNumber(wrappedValue: 10) var height: Int = 5 // Incorrect argument label in call (have 'wrappedValue:wrappedValue:', expected 'wrappedValue:maximum:')
+     @SmallNumber(wrappedValue: 10) var height: Int // Ok
+ }
+
+  // var myTestRectangle = TestRectangle(height: 30) // Cannot convert value of type 'Int' to expected argument type 'SmallNumber'
+ var myTestRectangle = TestRectangle()
+ print(myTestRectangle.height)
+ myTestRectangle.height = 20
+ print(myTestRectangle.height)
+
+ var narrowRectangle = NarrowRectangle()
+ print(narrowRectangle.height, narrowRectangle.width)
+ // Prints "2 3"
+
+ narrowRectangle.height = 100
+ narrowRectangle.width = 100
+ print(narrowRectangle.height, narrowRectangle.width)
+ // Prints "5 4"
+
+ struct MixedRectangle {
+     @SmallNumber var height: Int = 1
+     @SmallNumber(maximum: 9) var width: Int = 2
+ }
+
+ var mixedRectangle = MixedRectangle()
+ print(mixedRectangle.height)
+ // Prints "1"
+
+ mixedRectangle.height = 20
+ print(mixedRectangle.height)
+ // Prints "12"
+ */
+
+// MARK: Projecting a Value From a Property Wrapper
+/**
+ @propertyWrapper
+ struct SmallNumber2 {
+     private var number: Int
+     private(set) var projectedValue: Bool
+
+     var wrappedValue: Int {
+         get { return number }
+         set {
+             if newValue > 12 {
+                 number = 12
+                 projectedValue = true
+             } else {
+                 number = newValue
+                 projectedValue = false
+             }
+         }
+     }
+
+     init() {
+         self.number = 0
+         self.projectedValue = false
+     }
+ }
+ struct SomeStructure {
+     @SmallNumber2 var someNumber: Int
+ }
+ var someStructure = SomeStructure()
+
+ someStructure.someNumber = 4
+ print(someStructure.$someNumber)
+ // Prints "false"
+
+ someStructure.someNumber = 55
+ print(someStructure.$someNumber)
+ // Prints "true"
+
+ enum Size {
+     case small, large
+ }
+
+ struct SizedRectangle {
+     @SmallNumber2 var height: Int
+     @SmallNumber2 var width: Int
+
+     mutating func resize(to size: Size) -> Bool {
+         switch size {
+         case .small:
+             height = 10
+             width = 20
+         case .large:
+             height = 100
+             width = 100
+         }
+         return $height || $width
+     }
+ }
+
+ var sizedRectangle = SizedRectangle()
+
+ sizedRectangle.resize(to: .large)
+ sizedRectangle.height
+ sizedRectangle.width
+ sizedRectangle.resize(to: .small)
+ sizedRectangle.height
+
+ */
