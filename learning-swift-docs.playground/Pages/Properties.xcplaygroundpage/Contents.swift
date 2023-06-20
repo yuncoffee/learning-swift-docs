09c595ff8e6750a6641d0a19c88e45a44e8ecc7f
yuncoffee <dong072815@gmail.com>
2023-06-20 23:36:33 +0900

    study: Querying and Setting Type Properties

--- learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -614,3 +614,20 @@ OverideSomeClass.storedTypeProperty = "100"
 OverideSomeClass.storedTypeProperty
 
 OverideSomeClass.overrideableComputedTypeProperty
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+        didSet {
+            if currentLevel > AudioChannel.thresholdLevel {
+                // cap the new audio level to the threshold level
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                // store this as the new overall maximum input level
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
