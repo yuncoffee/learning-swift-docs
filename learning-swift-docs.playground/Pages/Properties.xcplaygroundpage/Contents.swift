afc98d11d076d12d9dcb4db28e1512dd6afba577
yuncoffee <dong072815@gmail.com>
2023-06-20 22:53:53 +0900

    study: Type Property Syntax

--- learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ learning-swift-docs.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -560,3 +560,33 @@ import Foundation
  someFunction()
 
  */
+
+// MARK: Type Property Syntax
+
+struct SomeStructure {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 1
+    }
+}
+enum SomeEnumeration {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 6
+    }
+}
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
+    }
+}
+
+SomeEnumeration.storedTypeProperty
+SomeEnumeration.computedTypeProperty
+
+SomeClass.overrideableComputedTypeProperty
+
